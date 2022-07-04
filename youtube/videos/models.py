3fb41a4899a41c6597e7c497f659e590988a780e
bikram.biswas <bikram.biswas@renewbuy.com>
2022-07-05 00:30:47 +0530

    Add video model, make migrations for video model

--- youtube/videos/models.py
+++ youtube/videos/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+#  Video title, description, publishing datetime, thumbnails URLs
+class Video(models.Model):
+    """
+    Model to store video_title, description and URL of the youtube video.
+    """
+
+    title = models.CharField("Title", max_length=300)
+    description = models.TextField()
+    published_on = models.DateTimeField()
+    thumbnail_url = models.URLField(max_length=255, help_text="URL of the thumbnail")
+    url = models.URLField(max_length=255, help_text="URL of the video")
+
+    class Meta:
+        verbose_name = "video"
+        verbose_name_plural = "videos"
+        ordering = ["-published_on"]
+
+    def __str__(self):
+        return self.title
